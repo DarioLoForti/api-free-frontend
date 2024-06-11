4a0122394df7a67c673ff64cd0cf35579fd8d8f1
Dario Lo Forti <lofortidario@gmail.com>
2024-06-11 09:48:20 +0200

    Add initial project files and dependencies

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="scss" scoped>
+  @use '../styles/generals.scss' as *;
+</style>
